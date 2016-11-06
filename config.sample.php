c0b04597adc2397d977071985e44969ac208843c
Ivor Scott <ivor@devpie.co>
2016-11-05 22:41:07 -0400

    allows for easy deployment

---
+++ config.sample.php
@@ -0,0 +1,18 @@
+<?php
+
+$GLOBALS['config'] = array(
+  'mysql' => array(
+    'host' 		=> 'localhost',
+    'username' 	=> 'root',
+    'password' 	=> '',
+    'db' 		=> 'collabvat'
+  ),
+  'remember' => array(
+    'cookie_name'	=> 'hash',
+    'cookie_expiry' =>  604800
+  ),
+  'session' => array(
+    'session_name'	=> 'user',
+    'token_name'	=> 'token'
+  )
+);
