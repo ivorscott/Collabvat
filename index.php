23c3a16ed25315643a6f2227a731d8699e946c68
Ivor Scott <ivor@devpie.co>
2016-11-06 01:04:25 -0400

    removed debugging lines

--- index.php
+++ index.php
@@ -12,28 +12,21 @@ ini_set('display_errors',1);
 error_reporting(-1);
 
 define('SCRIPT_BASE', realpath(dirname(__FILE__)));
-echo SCRIPT_BASE,"\n";
-// Ensure library is on include_path
 set_include_path(
     get_include_path() . PATH_SEPARATOR . SCRIPT_BASE
 );
+
 require_once  SCRIPT_BASE . "/config.php";
-echo SCRIPT_BASE . "/config.php","\n";
 require_once  SCRIPT_BASE . "/functions.php";
-echo SCRIPT_BASE . "/functions.php","\n";
 
 function autoload($class) {
-  echo SCRIPT_BASE . '/classes/' . $class . '.php',"\n";
   if(file_exists( SCRIPT_BASE . '/classes/' . $class . '.php')) {
     require_once  SCRIPT_BASE . '/classes/' . $class . '.php';
   } else {
     if(strstr($class,'Model')) {
-      echo "before explode class name is ", $class,"\n";
       $name = explode('Model', $class);
       $class = $name[0];
-      echo "after explode class name is ", $class,"\n";
     }
-    echo SCRIPT_BASE . '/models/' . strtolower($class) . '.php',"\n";
     require_once SCRIPT_BASE . '/models/' . strtolower($class) . '.php';
   }
 }
